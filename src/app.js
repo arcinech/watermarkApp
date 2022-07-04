58535f1a065eeaec25cf2c381888c988a4b323a9
Marcin Lech <arcinech@gmail.com>
2022-07-04 20:38:15 +0200

    Add: functions for watermark

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const Jimp = require('jimp');
+
+
+
+const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
+  const image = await Jimp.read(inputFile);
+  const font = await Jimp.loadFont(Jimp.FONT_SANS_128_BLACK);
+  const textData = {
+    text,
+    alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+    alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+  };
+
+  image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
+  await image.quality(100).writeAsync(outputFile);
+};
+
+const addImageWatermarkToImage = async function(inputFile, outputFile, watermarkFile) {
+  const image = await Jimp.read(inputFile);
+  const watermark = await Jimp.read(watermarkFile);
+
+  const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+  const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+
+  image.composite(watermark, x, y, {
+    mode: Jimp.BLEND_SOURCE_OVER,
+    opacitySource: 0.5,
+  });
+
+  await image.quality(100).writeAsync(outputFile);
+};
+
+addTextWatermarkToImage('./inputFiles/test.jpg', './outputFiles/test-with-watermark.jpg', 'Hello world');
+
+addImageWatermarkToImage('./inputFiles/test.jpg', './outputFiles/test-with-watermark2.jpg', './inputFiles/logo.png')
